3c3370958c9fa471c64f48b106014c966a38aaff
Leo Vandriel <git@leovandriel.com>
2022-01-04 23:06:35 -0800

    fix collision

--- src/collision.c
+++ src/collision.c
@@ -1,14 +1,16 @@
-#include "expression.h"
+#include "iterator.h"
 #include "table.h"
+#include "util.h"
 
 int main(int argc, char *argv[]) {
-    int size = 5;
+    int size = 6;
     ex_iterator stack[100];
     ht_table table;
     ht_init(&table, 10000);
     size_t count = 0;
+    char buffer[100];
     for (ex_init(stack, false); ex_next(stack) && stack->volume < size;) {
-        char *key = ex_double_str(stack->value);
+        char *key = ex_double_str(stack->value, buffer);
         char *value = ht_get(&table, key);
         char *expression = ex_iterator_str(stack);
         if (value && strcmp(key, "inf")) {
